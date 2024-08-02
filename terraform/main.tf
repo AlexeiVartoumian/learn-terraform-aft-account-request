de8830cca53a40d6a8047728c8f1150a09fbe7d7
AlexeiVartoumian <alex.vartoumian@gmail.com>
2024-08-02 18:36:02 +0100

    try

--- terraform/main.tf
+++ terraform/main.tf
@@ -27,7 +27,7 @@ module "sandbox" {
           "email-address" = "billing@mycompany.com",
           "name"          = "Account Receiveable",
           "phone-number"  = "+11234567890",
-          "title"         = "silly Billy Department"
+          "title"         = "sillyBilly Department"
         },
         "operations"= {
           "email-address" = "ops@mycompany.com",
